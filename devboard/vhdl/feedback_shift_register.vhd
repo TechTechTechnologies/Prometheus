4d574a736c5b6426586df268a5cb744387b9a2c5
Sam Boling <charles.samuel.boling@gmail.com>
2014-04-26 18:07:38 -0400

    checking in fsr before overhauling

--- devboard/vhdl/feedback_shift_register.vhd
+++ devboard/vhdl/feedback_shift_register.vhd
@@ -1,13 +1,14 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
-use IEEE.reduce_pack.all;
+use IEEE.std_logic_misc.all;
 
 entity FEEDBACK_SHIFT_REGISTER is
   generic
   (
-    function REDUCE (ARG : std_logic_vector) return std_logic := xor_reduce
-  )
+    DEPTH  : natural range 2 to natural'high := 8;
+    SEED   : std_logic_vector;
+  );
   port
   (
     CLOCK     : in  std_logic;
@@ -15,29 +16,31 @@ entity FEEDBACK_SHIFT_REGISTER is
 
     ENABLE    : in  std_logic;
     DIRECTION : in  std_logic;
-    BITS      : out std_logic_vector := (others => '0')
-    TAPS      : in  std_logic_vector(BITS'range)
+    BITS      : out std_logic_vector(DEPTH-1 downto 0);
+    TAPS      : in  std_logic_vector(DEPTH-1 downto 0)
   );
 end entity FEEDBACK_SHIFT_REGISTER;
 
 architecture BEHAVIORAL of FEEDBACK_SHIFT_REGISTER is
-  BITS_REG : std_logic_vector(BITS'range) := (others => '0');
+  signal BITS_REG : std_logic_vector(BITS'range) := SEED;
+  signal NEXT_BIT : std_logic := '0';
 begin
   
-  NEXT_BIT <= REDUCE(BITS_REG and TAPS);
+  NEXT_BIT <= xor_reduce(BITS_REG and TAPS);
+  BITS     <= BITS_REG;
   SHIFT_REGISTER : process (CLOCK) is
   begin
     if (rising_edge(CLOCK)) then
       if (RESET = '1') then
-        BITS_REG <= (others => '0');
+        BITS_REG <= SEED;
       else
         if (ENABLE = '1') then        
           if (DIRECTION = '1') then
-            BITS_REG                <= BITS_REG sll 1; 
-            BITS_REG(BITS_REG'right)  <= NEXT_BIT;
+            BITS_REG                 <= std_logic_vector(unsigned(BITS_REG) sll 1); 
+            BITS_REG(BITS_REG'right) <= NEXT_BIT;
           else
-            BITS_REG                <= BITS_REG srl 1;
-            BITS_REG(BITS_REG'left) <= NEXT_BIT;
+            BITS_REG                 <= std_logic_vector(unsigned(BITS_REG) srl 1); 
+            BITS_REG(BITS_REG'left)  <= NEXT_BIT;
           end if;
         else 
           BITS_REG <= BITS_REG;
--- devboard/vhdl/spi.vhd
+++ devboard/vhdl/spi.vhd
@@ -7,14 +7,15 @@ entity SPI_SERDES is
     
     CLOCK_DIVISOR   : natural range 1 to natural'high := 1;
     CLOCK_POLARITY  : std_logic := '0';
-    ENABLE_POLARITY : std_logic := '0'
+    ENABLE_POLARITY : std_logic := '0';
+    MSB_FIRST       : std_logic := '1'
   );
   port
   (
     CLOCK               : in std_logic;
     RESET               : in std_logic;
  
-    SERIAL_CLOCK        : out std_logic := '0';
+    SERIAL_CLOCK        : out std_logic := not CLOCK_POLARITY;
     SERIAL_ENABLE       : out std_logic := not ENABLE_POLARITY;
     SERIAL_MOSI         : out std_logic := '0';
     SERIAL_MISO         : in  std_logic;
@@ -28,29 +29,63 @@ entity SPI_SERDES is
 end entity;
 
 architecture BEHAVIORAL of SPI_SERDES is
-  type   INTERFACE_STATE_TYPE is (READY, XMIT);
-  signal INTERFACE_STATE : INTERFACE_STATE_TYPE := IDLE;
+  type   PARALLEL_INTERFACE_STATE_TYPE is (READY, XMIT);
+  signal PARALLEL_INTERFACE_STATE : INTERFACE_STATE_TYPE := READY;
 
-  signal DIVIDE_CLOCK    : 
+  type   SERIAL_INTERFACE_STATE_TYPE is (IDLE, CLK_LO, CLK_HI);
+  signal SERIAL_INTERFACE_STATE : SERIAL_INTERFACE_STATE_TYPE := IDLE;
+
 begin
 
-  PARALLEL_INTERFACE : process (CLOCK) is
+--  PARALLEL_INTERFACE : process (CLOCK) is
+--  begin
+--    if (rising_edge(CLOCK)) then
+--      if (RESET = '1') then
+--        INTERFACE_STATE     <= IDLE;
+--
+--      OUTGOING_WORD_VALID <= '0';
+--        OUTGOING_WORD       <= (others => '0');
+--      else
+--        case (INTERFACE_STATE) is
+--          when IDLE   =>
+--          when READY  =>
+--          when XMIT   =>
+--          when others =>
+--        end case;
+--      end if;
+--    end if;
+--  end process PARALLEL_INTERFACE; 
+
+  SERIAL_INTERFACE : process (CLOCK) is
   begin
     if (rising_edge(CLOCK)) then
       if (RESET = '1') then
-        INTERFACE_STATE     <= IDLE;
+        SERIAL_INTERFACE_STATE <= IDLE;
+        
+        SERIAL_CLOCK <= not CLOCK_POLARITY;
+        SERIAL_MOSI  <= '0';
 
-        OUTGOING_WORD_VALID <= '0';
-        OUTGOING_WORD       <= (others => '0');
+        TX_SHIFT_REG  <= (others => '0');
+        RX_SHIFT_REG  <= (others => '0');
+                
+        CLOCK_COUNTER <= (others => '0');
+        BIT_COUNTER   <= (others => '0');
       else
-        case (INTERFACE_STATE) is
-          when IDLE   =>
-          when READY  =>
-          when XMIT   =>
-          when others =>
+        case (SERIAL_INTERFACE_STATE) is
+          when IDLE =>
+            if (TRANSMISSION_ACTIVE = '1') then
+              SERIAL_INTERFACE_STATE <= CLK_LO;
+              TX_SHIFT_REG           <= OUTGOING_WORD;
+            else
+              SERIAL_INTERFACE_STATE <= IDLE;
+            end if;
+          when CLK_LO =>
+            if (MSB_FIRST = '1') then generate
+              TX_SHIFT_REG
+          when CLK_HI =>
         end case;
       end if;
     end if;
-  end process PARALLEL_INTERFACE; 
+  end process SERIAL_INTERFACE;
 
 end architecture BEHAVIORAL;
