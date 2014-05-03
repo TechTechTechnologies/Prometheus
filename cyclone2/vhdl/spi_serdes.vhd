30bf4bd6bf9921f62154a8267cd0ee592d105dab
Sam Boling <charles.samuel.boling@gmail.com>
2014-05-03 14:06:06 -0400

    bidirectional SPI working with basic master/slave modes

--- cyclone2/vhdl/spi_serdes.vhd
+++ cyclone2/vhdl/spi_serdes.vhd
@@ -24,10 +24,10 @@ entity SPI_SERDES is
     CLOCK               : in std_logic;
     RESET               : in std_logic;
  
-    SERIAL_CLOCK        : out std_logic := not(CLOCK_POLARITY);
-    SERIAL_ENABLE       : out std_logic := not(ENABLE_POLARITY);
-    SERIAL_MOSI         : out std_logic := '0';
-    SERIAL_MISO         : in  std_logic;
+    SERIAL_CLOCK        : inout std_logic := not(CLOCK_POLARITY);
+    SERIAL_ENABLE       : inout std_logic := not(ENABLE_POLARITY);
+    SERIAL_MOSI         : inout std_logic := '0';
+    SERIAL_MISO         : inout std_logic := '0';
 
     READY_FOR_DATA      : out std_logic;
     OUTGOING_WORD_VALID : in  std_logic;
@@ -49,10 +49,13 @@ architecture BEHAVIORAL of SPI_SERDES is
   signal INCOMING_WORD_VALID_I : std_logic := '0';
   signal INCOMING_WORD_I       : std_logic_vector(INCOMING_WORD'range) := (others => '0');
   
-  signal LOAD_SHIFT_REGS : std_logic := '0';
-  signal STEP_SHIFT_REGS : std_logic := '0'; 
+  signal LOAD_SHIFT_REGS  : std_logic := '0';
+  signal CLEAR_SHIFT_REGS : std_logic := '0';
+  signal STEP_SHIFT_REGS  : std_logic := '0'; 
   signal TX_BITS : std_logic_vector(PARALLEL_WIDTH-1 downto 0) := (others => '0');
   signal RX_BITS : std_logic_vector(PARALLEL_WIDTH-1 downto 0) := (others => '0');
+  signal INCOMING_BIT : std_logic := '0';
+  signal OUTGOING_BIT : std_logic := '0';
   
   signal CLOCK_COUNTER : std_logic_vector(vector_length(CLOCK_DIVIDER)-1  downto 0) := (others => '0');
   signal BIT_COUNTER   : std_logic_vector(vector_length(PARALLEL_WIDTH)-1 downto 0) := (others => '0');
@@ -111,7 +114,11 @@ begin
                 PARALLEL_INTERFACE_STATE <= BUSY;
               end if;
             else
-              PARALLEL_INTERFACE_STATE <= IDLE;
+              if (LOAD_SHIFT_REGS = '1') then
+                PARALLEL_INTERFACE_STATE <= IDLE;
+              else 
+                PARALLEL_INTERFACE_STATE <= BUSY;
+              end if;
               if (OUTGOING_WORD_VALID =  '1') then
                 WORD_REGISTER <= OUTGOING_WORD;
               end if;
@@ -140,10 +147,7 @@ begin
         when IDLE =>
           READY_FOR_DATA_I <= '1';
           WORD_PENDING     <= '0';
-        when BUSY =>
-          READY_FOR_DATA_I <= '1';
-          WORD_PENDING     <= '1';
-        when FULL =>
+        when others =>
           READY_FOR_DATA_I <= '0';
           WORD_PENDING     <= '1';
       end case;
@@ -163,16 +167,17 @@ begin
           BIT_COUNTER   <= (others => '0');
 
           INCOMING_WORD_I <= (others => '0');
+
+          CLEAR_SHIFT_REGS <= '0';
         else
           case (SERIAL_INTERFACE_STATE) is
             when IDLE =>
               if (WORD_PENDING = '1') then
                 SERIAL_INTERFACE_STATE <= CLK_LO;
-                LOAD_SHIFT_REGS        <= '1';
               else
                 SERIAL_INTERFACE_STATE <= IDLE;
-                LOAD_SHIFT_REGS        <= '0';
               end if;
+              CLEAR_SHIFT_REGS <= '0';
             when CLK_LO =>
               if (CLOCK_COUNTER = CLOCK_DIVIDER-1) then
                 SERIAL_INTERFACE_STATE <= CLK_HI;
@@ -181,7 +186,7 @@ begin
                 SERIAL_INTERFACE_STATE <= CLK_LO;
                 CLOCK_COUNTER          <= CLOCK_COUNTER + 1;
               end if;
-              LOAD_SHIFT_REGS <= '0';
+              CLEAR_SHIFT_REGS <= '0';
             when CLK_HI =>
               if (CLOCK_COUNTER = CLOCK_DIVIDER-1) then
                 if (BIT_COUNTER = PARALLEL_WIDTH-1) then
@@ -189,28 +194,29 @@ begin
                   BIT_COUNTER <= (others => '0');
 
                   INCOMING_WORD_I <= RX_BITS;
+                  CLEAR_SHIFT_REGS <= '1';
                 else
                   SERIAL_INTERFACE_STATE <= CLK_LO;
                   BIT_COUNTER <= BIT_COUNTER + 1;
+                  CLEAR_SHIFT_REGS <= '0';
                 end if;
                 CLOCK_COUNTER          <= (others => '0');
               else
                 SERIAL_INTERFACE_STATE <= CLK_HI;
                 CLOCK_COUNTER          <= CLOCK_COUNTER + 1;
+                CLEAR_SHIFT_REGS <= '0';
               end if;
-              LOAD_SHIFT_REGS <= '0';
           end case;
         end if;
       end if;
     end process SERIAL_INTERFACE;
 
-    GENERATE_SPI_SLAVE :
-    if (not MASTER) generate -- TODO
-    end generate GENERATE_SPI_SLAVE;
-
     SERIAL_ENABLE <=      ENABLE_POLARITY 
                      when (SERIAL_BUSY = '1') 
                      else not(ENABLE_POLARITY);
+    SERIAL_MOSI  <= OUTGOING_BIT;
+    SERIAL_MISO  <= 'Z';
+    INCOMING_BIT <= SERIAL_MISO;
     SERIAL_INTERFACE_COMBINATIONAL : 
     process (RESET, SERIAL_INTERFACE_STATE) is
     begin
@@ -226,6 +232,7 @@ begin
             STEP_SHIFT_REGS <= '0';
           when CLK_LO =>
             SERIAL_CLOCK    <= not(CLOCK_POLARITY);
+
             SERIAL_BUSY     <= '1';
             STEP_SHIFT_REGS <= '1';
           when CLK_HI =>
@@ -235,18 +242,121 @@ begin
         end case;
       end if;
     end process SERIAL_INTERFACE_COMBINATIONAL;
-
-    GENERATE_LSB_FIRST :
-    if (LSB_FIRST = '1') generate
-      SERIAL_MOSI <= TX_BITS(TX_BITS'right);
-    end generate GENERATE_LSB_FIRST;
-
-    GENERATE_MSB_FIRST :
-    if (LSB_FIRST = '0') generate
-      SERIAL_MOSI <= TX_BITS(TX_BITS'left);
-    end generate GENERATE_MSB_FIRST;
   end generate GENERATE_SPI_MASTER;
 
+  GENERATE_SPI_SLAVE :
+  if (not MASTER) generate
+    SERIAL_INTERFACE :
+    process (CLOCK) is
+    begin
+      if (rising_edge(CLOCK)) then
+        if (RESET = '1') then
+           SERIAL_INTERFACE_STATE <= IDLE;
+           CLOCK_COUNTER <= (others => '0');
+           BIT_COUNTER   <= (others => '0');
+
+           INCOMING_WORD_I <= (others => '0');
+           CLEAR_SHIFT_REGS <= '1';
+        else
+          case (SERIAL_INTERFACE_STATE) is
+            when IDLE =>
+              if (SERIAL_ENABLE = ENABLE_POLARITY) then
+                SERIAL_INTERFACE_STATE <= CLK_LO;
+              else
+                SERIAL_INTERFACE_STATE <= IDLE;
+              end if;
+              CLEAR_SHIFT_REGS <= '0';
+
+           when CLK_LO =>
+             if (SERIAL_ENABLE = not(ENABLE_POLARITY)) then
+               SERIAL_INTERFACE_STATE <= IDLE;
+               INCOMING_WORD_I        <= RX_BITS;
+               CLEAR_SHIFT_REGS       <= '1';
+             else
+               if (SERIAL_CLOCK = CLOCK_POLARITY) then
+                 SERIAL_INTERFACE_STATE <= CLK_HI;
+               else
+                 SERIAL_INTERFACE_STATE <= CLK_LO;
+               end if;
+               CLEAR_SHIFT_REGS <= '0';
+             end if;
+
+           when CLK_HI =>
+             if (SERIAL_ENABLE = not(ENABLE_POLARITY)) then
+               SERIAL_INTERFACE_STATE <= IDLE;
+               INCOMING_WORD_I        <= RX_BITS;
+               CLEAR_SHIFT_REGS       <= '1';
+             else
+               if (SERIAL_CLOCK = not(CLOCK_POLARITY)) then
+                 SERIAL_INTERFACE_STATE <= CLK_LO;
+               else
+                 SERIAL_INTERFACE_STATE <= CLK_HI;
+               end if;
+               CLEAR_SHIFT_REGS <= '0';
+             end if;
+          end case;
+        end if;
+      end if;
+    end process SERIAL_INTERFACE;
+
+    ADVANCE_SHIFT_REGS :
+    process (CLOCK) is
+    begin
+      if (rising_edge(CLOCK)) then
+        if (RESET = '1') then
+          STEP_SHIFT_REGS <= '0';
+        else
+          if (SERIAL_ENABLE = ENABLE_POLARITY) then
+            if (SERIAL_CLOCK = CLOCK_POLARITY) then
+              if (STEP_SHIFT_REGS = '0') then
+                STEP_SHIFT_REGS <= '1';
+              else
+                STEP_SHIFT_REGS <= '0';
+              end if;
+            else
+              STEP_SHIFT_REGS <= '0';
+            end if;
+          else
+            STEP_SHIFT_REGS <= '0';
+          end if;
+        end if;
+      end if;
+    end process ADVANCE_SHIFT_REGS;
+
+    SERIAL_ENABLE <= 'Z';
+    SERIAL_CLOCK  <= 'Z';
+    SERIAL_MOSI   <= 'Z';
+    SERIAL_MISO   <= OUTGOING_BIT;
+    INCOMING_BIT  <= SERIAL_MOSI;
+    SERIAL_INTERFACE_COMBINATIONAL : 
+    process (RESET, SERIAL_INTERFACE_STATE) is
+    begin
+      if (RESET = '1') then
+        SERIAL_BUSY     <= '0';
+      else
+        case (SERIAL_INTERFACE_STATE) is
+          when IDLE   =>
+            SERIAL_BUSY <= '0';
+          when CLK_LO =>
+            SERIAL_BUSY <= '1';
+          when CLK_HI =>
+            SERIAL_BUSY <= '1';
+        end case;
+      end if;
+    end process SERIAL_INTERFACE_COMBINATIONAL;    
+  end generate GENERATE_SPI_SLAVE;
+
+  GENERATE_LSB_FIRST :
+  if (LSB_FIRST = '1') generate
+    OUTGOING_BIT <= TX_BITS(TX_BITS'right);
+  end generate GENERATE_LSB_FIRST;
+
+  GENERATE_MSB_FIRST :
+  if (LSB_FIRST = '0') generate
+    OUTGOING_BIT <= TX_BITS(TX_BITS'left);
+  end generate GENERATE_MSB_FIRST;
+
+  LOAD_SHIFT_REGS <= WORD_PENDING and not(SERIAL_BUSY);
   OUTGOING_SHIFT_REGISTER : 
   SHIFT_REGISTER
     generic map
@@ -280,13 +390,13 @@ begin
       CLOCK     => CLOCK,
       RESET     => RESET,
 
-      LOAD      => '0',
+      LOAD      => LOAD_SHIFT_REGS,
       BITS_IN   => (others => '0'),
 
       STEP      => STEP_SHIFT_REGS,
       DIRECTION => '0',
       BITS_OUT  => RX_BITS,
-      NEXT_BIT  => SERIAL_MISO
+      NEXT_BIT  => INCOMING_BIT
     );
 
 end architecture BEHAVIORAL;
--- cyclone2/vhdl/spi_testbench.vhd
+++ cyclone2/vhdl/spi_testbench.vhd
@@ -1,8 +1,13 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
 
 entity SPI_TESTBENCH is
+  generic
+  (
+    MASTER : boolean := true
+  );
 begin
 end;
 
@@ -15,6 +20,8 @@ architecture SIMULATION of SPI_TESTBENCH is
   signal SERIAL_ENABLE  : std_logic := '1';
   signal SERIAL_MOSI    : std_logic := '0';
   signal SERIAL_MISO    : std_logic := '0';
+
+  signal BIT_COUNTER    : std_logic_vector(3 downto 0) := (others => '0');
   
   signal READY_FOR_DATA      : std_logic := '0';
   signal WORD_SENDABLE       : std_logic := '0';
@@ -41,10 +48,10 @@ architecture SIMULATION of SPI_TESTBENCH is
       CLOCK               : in std_logic;
       RESET               : in std_logic;
  
-      SERIAL_CLOCK        : out std_logic := not CLOCK_POLARITY;
-      SERIAL_ENABLE       : out std_logic := not ENABLE_POLARITY;
-      SERIAL_MOSI         : out std_logic := '0';
-      SERIAL_MISO         : in  std_logic;
+      SERIAL_CLOCK        : inout std_logic := not CLOCK_POLARITY;
+      SERIAL_ENABLE       : inout std_logic := not ENABLE_POLARITY;
+      SERIAL_MOSI         : inout std_logic := '0';
+      SERIAL_MISO         : inout std_logic;
 
       READY_FOR_DATA      : out std_logic;
       OUTGOING_WORD_VALID : in  std_logic;
@@ -56,22 +63,6 @@ begin
   
   CLOCK <= not CLOCK after (CLOCK_PERIOD/2);
   RESET <= '0' after (3*CLOCK_PERIOD);
-  
-  TOGGLE_MISO :
-  process (CLOCK) is
-  begin
-    if (rising_edge(CLOCK)) then
-      if (RESET = '1') then
-        SERIAL_MISO <= '0';
-      else
-        if (SERIAL_CLOCK = '0') then
-          SERIAL_MISO <= not(SERIAL_MISO);
-        else
-          SERIAL_MISO <= SERIAL_MISO;
-        end if;
-      end if;
-    end if;
-  end process TOGGLE_MISO;
   
   STROBE_VALID :
   process (CLOCK) is
@@ -85,8 +76,74 @@ begin
     end if;
   end process STROBE_VALID;
   OUTGOING_WORD_VALID <= WORD_SENDABLE and READY_FOR_DATA;
-  OUTGOING_WORD       <= X"A5";
+  OUTGOING_WORD       <= X"DB";
   
+  GENERATE_MASTER_SIGNALS :
+  if (MASTER) generate
+    TOGGLE_MISO :
+    process (CLOCK) is
+    begin
+      if (rising_edge(CLOCK)) then
+        if (RESET = '1') then
+          SERIAL_MISO <= '0';
+        else
+          if (SERIAL_CLOCK = '0') then
+            SERIAL_MISO <= not(SERIAL_MISO);
+          else
+            SERIAL_MISO <= SERIAL_MISO;
+          end if;
+        end if;
+      end if;
+    end process TOGGLE_MISO;
+  end generate GENERATE_MASTER_SIGNALS;
+
+  GENERATE_SLAVE_SIGNALS :
+  if (not MASTER) generate
+    ASSERT_ENABLE :
+    process (CLOCK) is
+    begin
+      if (rising_edge(CLOCK)) then
+        if (RESET = '1') then
+          BIT_COUNTER   <= (others => '0');
+          SERIAL_ENABLE <= '1';
+          SERIAL_CLOCK  <= '0';
+        else
+          if (BIT_COUNTER = 9) then
+            BIT_COUNTER   <= (others => '0');
+            SERIAL_ENABLE <= '1';
+          else
+            BIT_COUNTER   <= BIT_COUNTER + 1;
+            SERIAL_ENABLE <= '0';
+          end if;
+
+          if (SERIAL_ENABLE = '0') then
+            SERIAL_CLOCK <= not SERIAL_CLOCK;
+          end if;
+        end if;
+      end if;
+    end process ASSERT_ENABLE;
+
+    TOGGLE_MOSI :
+    process (CLOCK) is
+    begin
+      if (rising_edge(CLOCK)) then
+        if (RESET = '1') then
+          SERIAL_MOSI <= '0';
+        else
+          if (SERIAL_ENABLE = '0') then
+            if (SERIAL_CLOCK = '0') then
+              SERIAL_MOSI <= not(SERIAL_MOSI);
+            else
+              SERIAL_MOSI <= SERIAL_MOSI;
+            end if;
+          else
+            SERIAL_MOSI <= '0';
+          end if;
+        end if;
+      end if;
+    end process TOGGLE_MOSI;
+  end generate GENERATE_SLAVE_SIGNALS;
+
   DUT :
   SPI_SERDES 
     generic map
@@ -99,7 +156,7 @@ begin
       CLOCK_POLARITY  => '0',
       ENABLE_POLARITY => '0',
 
-      MASTER          => true,
+      MASTER          => MASTER,
       LSB_FIRST       => '1'
     )
     port map
